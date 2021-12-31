516e8fe1cd15b362662e165b0a0e7e1835a4fe44
Максим Янин <yankidevel@gmail.com>
2021-12-31 14:31:58 +0300

    GO. Уровень 1. Домашнее задание 3. Работа над ошибками

--- golang1/lesson3/2/main.go
+++ golang1/lesson3/2/main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 	"os"
 )
 
 func main() {
-	var a int64
+	var a int
 	fmt.Println("Введите целое число:")
 	_, err := fmt.Scan(&a)
 	if err != nil {
 		fmt.Println("Число должно быть целым!")
 		os.Exit(1)
 	}
-	if big.NewInt(a).ProbablyPrime(0) {
-		fmt.Println("Число ", a, " простое")
-		os.Exit(1)
+	for i := 2; i < a; i++{
+		if a % i == 0 {
+			fmt.Println("Число ", a, " не простое")
+			os.Exit(1)
+		}
 	}
-	fmt.Println("Число ", a, " не простое")
+	fmt.Println("Число ", a, " простое")
 }
