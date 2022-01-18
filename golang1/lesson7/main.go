9ef13f68fb0e7a85e224bfd91341d60c05183513
Максим Янин <yankidevel@gmail.com>
2022-01-18 21:13:38 +0300

    GO. Уровень 1. Домашнее задание 7

---
+++ golang1/lesson7/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	/**
+	* 1. Несколько примеров из прошлых уроков, принимаются интерфейсы:
+	fmt.Println()
+	fmt.Scan()
+	fmt.Scanln()
+	fmt.Printf()
+	fmt.Errorf()
+	*
+	**/
+}
